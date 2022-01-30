d178b987a805c455dca31be4e6c79a0df9696f97
aditi-cpu <78260211+aditi-cpu@users.noreply.github.com>
2022-01-30 15:04:51 +0530

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEU-lIgVqNXZLHsT46tw_yC_4nqtkIdD0",
+  authDomain: "robotmaze1.firebaseapp.com",
+  databaseURL: "https://robotmaze1-default-rtdb.firebaseio.com",
+  projectId: "robotmaze1",
+  storageBucket: "robotmaze1.appspot.com",
+  messagingSenderId: "424157058648",
+  appId: "1:424157058648:web:ca8549e8329cbb7e121c2d"
+};
